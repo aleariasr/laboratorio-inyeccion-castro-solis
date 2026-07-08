18597c9c30357beec74128e53dd4a3e48520b831
Alejandro Arias <aleariasrojas@hotmail.com>
2026-07-07 18:36:21 -0600

    feat(accounts): enable DRF token authentication

--- backend/src/config/settings/base.py
+++ backend/src/config/settings/base.py
@@ -39,6 +39,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 LOCAL_APPS = [
@@ -147,7 +148,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
